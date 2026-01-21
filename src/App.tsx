2d2eef59a0e267bd83c149de4aa51642ef271142
krish-mamtora <krish.mamtora@roimaint.com>
2026-01-21 10:34:56 +0530

    added conditoin in price to categories them

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import './index.css'
 import { useForm } from 'react-hook-form'
 import Dashboard from './pages/Dashboard'
 import Navbar from './components/Navbar'
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import '../App.css'
+import '../index.css'
 import { set, useForm } from 'react-hook-form'
 
 function Dashboard() {
@@ -29,6 +30,7 @@ function Dashboard() {
       ProductName: data.ProductName,
       Price : data.Price,
       Category : data.Category,
+      Stock : data.Stock,
     };
 
     const updated = [...olditems , newItem];
@@ -61,20 +63,24 @@ function Dashboard() {
           localStorage.setItem('items' , JSON.stringify(allitems));
            setItems(allitems);
     }
+
   return (
     <>
     
     <div>
-     
       <div >
         <h4>Dashboard</h4>
         <div >
            <h5>Items</h5>
-           <div>
+           <div style={{display:"grid" , gap :"2px"}}>
            <ul>
+            <h4>Product Name  | Category | Price | Stock</h4>
             { items.map((item , index) => (
-                <li key={index}>
-                  {item.ProductName} - {item.Category} - {item.Price}
+                <li key={index} style={{border : "1px solid black" ,gap:"1px"}}>
+                  {item.ProductName} - {item.Category} - {item.Price} - {item.Stock}
+                  {item.Price<=100 && <p>Affordable</p>}
+                  {item.Price>100 && item.Price<1000 && <p>Medium Range</p>}
+                  {item.Price>=1000 && <p>Costly {item.Category}</p>}
                   <button onClick={() => deleteItem(index)}>Delete</button>
                 </li>
              ))}
@@ -83,15 +89,24 @@ function Dashboard() {
         </div>
         <div>
           <h5>Form</h5>
-          <div style={{border:"1px solid black"}}>
+          <div style={{border:"1px solid black", backgroundColor:"lightblue"}}>
             <form  onSubmit={handleSubmit(onSubmit)}>
+              <br />
                 <span>Product Name : </span>
                 <input {...register("ProductName" , {required : true})} placeholder='Product Name' /> <br />
                 <span>Category : </span>
-                <input {...register("Category" , {required : true})} placeholder='Category' /> <br />
+                <select{...register("Category" , {required : true})}>
+                  <option value="">Select Category</option>
+                  <option value="Fashion">Fashion</option>
+                  <option value="Electronic">Electronic</option>
+                  <option value="Home">Home</option>
+                  <option value="Grocery">Grocery</option>
+                </select><br />
                 <span>Price : </span>
-                <input {...register("Price" , {required : true})} placeholder='Price'/> <br />
-             <button type='submit'>Add Item</button>
+                <input {...register("Price" , {required : true})} type='number' placeholder='Price'/> <br />
+                <span>Stock : </span>
+                <input  {...register("Stock" , {required: true})}type="number" placeholder='Stock' /> <br />
+             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"  type='submit'>Add Item</button>
             </form>
           </div>
         </div>
